8788d71ac663c8e6d020cc4c4aca80f97e0b26d2
Quoc Vuong <quocvuongle.ct@gmail.com>
2024-10-14 22:20:31 +0700

    fe: setup website

---
+++ app/client/website/src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { IBM_Plex_Mono } from "next/font/google";
+import React from "react";
+
+import "./globals.css";
+import "../styles/prism.css";
+import "animate.css";
+import { ThemeProvider } from "@/context/ThemeProvider";
+
+const imbPlexMono = IBM_Plex_Mono({
+    weight: ["400", "500", "600", "700"],
+    style: ["normal", "italic"],
+    subsets: ["latin", "vietnamese"],
+    display: "swap",
+});
+
+export const metadata: Metadata = {
+    title: "Tastopia",
+    description: "About Tastopia",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={`${imbPlexMono.className} h-screen w-screen`}>
+                <ThemeProvider>{children}</ThemeProvider>
+            </body>
+        </html>
+    );
+}
