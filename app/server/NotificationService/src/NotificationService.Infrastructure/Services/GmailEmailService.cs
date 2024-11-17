91f21347bf6a495bcf439fc20f6bc02cc157096d
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2024-11-17 23:49:52 +0700

    be: init gmail service

---
+++ app/server/NotificationService/src/NotificationService.Infrastructure/Services/GmailEmailService.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace NotificationService.Infrastructure.Services;
+
+public class GmailEmailService
+{
+    private readonly string SMTPServer = "smtp.gmail.com";
+    private readonly int SMTPPort = 587;
+    private string EmailAddress;
+    private string EmailPassword;
+
+    public GmailEmailService(string emailAddress, string emailPassword)
+    {
+        EmailAddress = emailAddress;
+        EmailPassword = emailPassword;
+    }
+
+    public async Task SendEmail(string emailTo, string subject, string body, bool isHtml = false)
+    {
+        using var smtpClient = new SmtpClient(SMTPServer, SMTPPort)
+        {
+            Credentials = new NetworkCredential(EmailAddress, EmailPassword),
+            EnableSsl = true
+        };
+
+        var mailMessage = new MailMessage
+        {
+            From = new MailAddress(EmailAddress),
+            Subject = subject,
+            Body = body,
+            IsBodyHtml = isHtml
+        };
+
+        mailMessage.To.Add(emailTo);
+
+        await smtpClient.SendMailAsync(mailMessage);
+    }
+}
