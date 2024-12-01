b88c2690ce200fc1bf1a4cc4bd47fd7425bf1e9b
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2024-12-01 23:55:24 +0700

    be: add email worker service

---
+++ app/server/NotificationService/src/EmailWorker/Program.cs
@@ -0,0 +1,24 @@
+using EmailWorker;
+using EmailWorker.Utilities;
+
+EnvUtility.LoadEnvFile();
+
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        services.AddSingleton(provider => new RabbitMQProvider(new RabbitMQ.Client.ConnectionFactory
+        {
+            HostName = DotNetEnv.Env.GetString("RABBITMQ_HOST", "localhost"),
+            Port = 5672,
+            UserName = DotNetEnv.Env.GetString("RABBITMQ_DEFAULT_USER", "admin"),
+            Password = DotNetEnv.Env.GetString("RABBITMQ_DEFAULT_PASS", "pass"),
+            VirtualHost = DotNetEnv.Env.GetString("RABBITMQ_DEFAULT_VHOST", "/"),
+            AutomaticRecoveryEnabled = true,
+            NetworkRecoveryInterval = TimeSpan.FromSeconds(10)
+        }, provider.GetRequiredService<ILogger<RabbitMQProvider>>()));
+
+        services.AddHostedService<Worker>();
+    });
+
+var host = builder.Build();
+host.Run();
