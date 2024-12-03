779338a744b8eb8c0e13c28c5446e4fa5e099924
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2024-12-03 23:26:57 +0700

    o: detect docker active in setup backend

--- scripts/setup-backend.sh
+++ scripts/setup-backend.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 # Define color
+LIGHT_RED='\033[1;31m'
 GREEN='\033[0;32m'
 GREEN_OCT='\o033[0;32m'
 NC='\033[0m'      # No Color
-NC_OCT='\o033[0m' # No Color
+
+if ! docker info > /dev/null 2>&1; then
+    printf "\n\t${LIGHT_RED}*** Docker is not running ❌${NC} *** . Exiting the script.\n\n"
+    exit 1
+fi
 
 dotnet tool install --global dotnet-ef --version 9.0.0
 
