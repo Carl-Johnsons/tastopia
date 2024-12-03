2b957d9d000eed717ec16cd1465b3c1ae6c3b60a
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2024-12-03 23:41:10 +0700

    o: update script

--- scripts/restore-all-services.sh
+++ scripts/restore-all-services.sh
@@ -29,14 +29,14 @@ restore_service() {
   cd "$project_root"
 }
 
+# Publishing Contract solution
+echo -e "\e[95mPublishing Contract solution ...\e[0m"
+(cd ./app/server/Contract/Contract && dotnet publish)
+cd "$project_root"
+
 restore_service "./app/server/APIGateway/src/APIGateway" "api gateway" && \
 restore_service "./app/server/IdentityService/src/DuendeIdentityServer" "identity" && \
 restore_service "./app/server/UploadFileService/src/UploadFileService.API" "upload" && \
 restore_service "./app/server/UserService/src/UserService.API" "user" && \
 restore_service "./app/server/NotificationService/src/NotificationService.API" notification && \
 restore_service "./app/server/SignalRService/src/SignalRHub" "signalR"
-
-# Publishing Contract solution
-echo -e "\e[95mPublishing Contract solution ...\e[0m"
-(cd ./app/server/Contract/Contract && dotnet publish)
-cd "$project_root"
