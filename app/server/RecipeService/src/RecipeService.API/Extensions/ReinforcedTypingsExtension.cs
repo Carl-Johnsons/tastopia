4037373996efe49196fc86ed9a8cc323055e2d14
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2025-01-12 21:44:58 +0700

    be: remove generating export interface

--- app/server/RecipeService/src/RecipeService.API/Extensions/ReinforcedTypingsExtension.cs
+++ app/server/RecipeService/src/RecipeService.API/Extensions/ReinforcedTypingsExtension.cs
@@ -13,8 +13,7 @@ public static class ReinforcedTypingsExtension
         {
             config.CamelCaseForProperties()
                   .AutoOptionalProperties()
-                  .ExportPureTypings(typings: true)
-                  .UseModules(useModules: true, discardNamespaces: true); 
+                  .ExportPureTypings(typings: true); 
         });
 
         // Substitute C# type to typescript type
