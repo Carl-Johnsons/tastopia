ecc0a64ddb24277a11fda1cc17d6d1f1edd7b7fe
Ba Thinh <thefanofanimeromanceandcomedy@gmail.com>
2025-01-13 22:46:15 +0700

    be: set view recipe detail limit to 20

--- app/server/TrackingService/src/TrackingService.Domain/Constants/UserViewRecipeDetail.constant.cs
+++ app/server/TrackingService/src/TrackingService.Domain/Constants/UserViewRecipeDetail.constant.cs
@@ -2,5 +2,5 @@
 
 public static class UserViewRecipeDetailConstant
 {
-    public static readonly int USER_VIEW_RECIPE_DETAIL_LIMIT = 2;
+    public static readonly int USER_VIEW_RECIPE_DETAIL_LIMIT = 20;
 }
