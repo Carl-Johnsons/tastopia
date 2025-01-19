de149e7849a2a7d5c97596e538e698abfb09b9d2
Phen Chua <89496400+PhenChua29@users.noreply.github.com>
2025-01-19 15:06:59 +0700

    fe: add secondary font
    There are 2 things included in this commit:
    
    - Secondary font is added.
    - The tailwind config is refactored so that the fonts declaring is
    abstracted out of the config object to make it easy on the eye. However,
    somehow the fontFamily constant cannot be in other file and imported
    into the tailwind config file without causing the typescript's
    intellisense function to not working correctly. Therefore, the font
    constant object is staying the config file for now.

---
+++ app/client/mobile/constants/fonts.ts
@@ -0,0 +1,39 @@
+export const SOPHIA_PRO = {
+  "Sofia Pro Black": require("../assets/fonts/sophiaPro/Sofia-Pro-Black-Az.otf"),
+  "Sofia Pro Black Italic": require("../assets/fonts/sophiaPro/Sofia-Pro-Black-Italic-Az.otf"),
+  "Sofia Pro Bold": require("../assets/fonts/sophiaPro/Sofia-Pro-Bold-Az.otf"),
+  "Sofia Pro Bold Italic": require("../assets/fonts/sophiaPro/Sofia-Pro-Bold-Italic-Az.otf"),
+  "Sofia Pro ExtraLight": require("../assets/fonts/sophiaPro/Sofia-Pro-ExtraLight-Az.otf"),
+  "Sofia Pro ExtraLight Italic": require("../assets/fonts/sophiaPro/Sofia-Pro-ExtraLight-Italic-Az.otf"),
+  "Sofia Pro Light": require("../assets/fonts/sophiaPro/Sofia-Pro-Light-Az.otf"),
+  "Sofia Pro Light Italic": require("../assets/fonts/sophiaPro/Sofia-Pro-Light-Italic-Az.otf"),
+  "Sofia Pro Medium": require("../assets/fonts/sophiaPro/Sofia-Pro-Medium-Az.otf"),
+  "Sofia Pro Medium Italic": require("../assets/fonts/sophiaPro/Sofia-Pro-Medium-Italic-Az.otf"),
+  "Sofia Pro Regular": require("../assets/fonts/sophiaPro/Sofia-Pro-Regular-Az.otf"),
+  "Sofia Pro Regular Italic": require("../assets/fonts/sophiaPro/Sofia-Pro-Regular-Italic-Az.otf"),
+  "Sofia Pro Semi Bold": require("../assets/fonts/sophiaPro/Sofia-Pro-Semi-Bold-Az.otf"),
+  "Sofia Pro Semi Bold Italic": require("../assets/fonts/sophiaPro/Sofia-Pro-Semi-Bold-Italic-Az.otf"),
+  "Sofia Pro UltraLight": require("../assets/fonts/sophiaPro/Sofia-Pro-UltraLight-Az.otf"),
+  "Sofia Pro UltraLight Italic": require("../assets/fonts/sophiaPro/Sofia-Pro-UltraLight-Italic-Az.otf")
+};
+
+export const HELVETICA_NEUE = {
+  "Helvetica Neue Black": require("../assets/fonts/helveticaNeue/HelveticaNeueBlack.otf"),
+  "Helvetica Neue Black Italic": require("../assets/fonts/helveticaNeue/HelveticaNeueBlackItalic.otf"),
+  "Helvetica Neue Bold": require("../assets/fonts/helveticaNeue/HelveticaNeueBold.otf"),
+  "Helvetica Neue Bold Italic": require("../assets/fonts/helveticaNeue/HelveticaNeueBoldItalic.otf"),
+  "Helvetica Neue Heavy": require("../assets/fonts/helveticaNeue/HelveticaNeueHeavy.otf"),
+  "Helvetica Neue Heavy Italic": require("../assets/fonts/helveticaNeue/HelveticaNeueHeavyItalic.otf"),
+  "Helvetica Neue Italic": require("../assets/fonts/helveticaNeue/HelveticaNeueItalic.ttf"),
+  "Helvetica Neue Light": require("../assets/fonts/helveticaNeue/HelveticaNeueLight.otf"),
+  "Helvetica Neue Light Italic": require("../assets/fonts/helveticaNeue/HelveticaNeueLightItalic.otf"),
+  "Helvetica Neue Medium": require("../assets/fonts/helveticaNeue/HelveticaNeueMedium.otf"),
+  "Helvetica Neue Medium Italic": require("../assets/fonts/helveticaNeue/HelveticaNeueMediumItalic.otf"),
+  "Helvetica Neue Roman": require("../assets/fonts/helveticaNeue/HelveticaNeueRoman.otf"),
+  "Helvetica Neue Thin": require("../assets/fonts/helveticaNeue/HelveticaNeueThin.otf"),
+  "Helvetica Neue Thin Italic": require("../assets/fonts/helveticaNeue/HelveticaNeueThinItalic.otf"),
+  "Helvetica Neue UltraLight": require("../assets/fonts/helveticaNeue/HelveticaNeueUltraLight.otf"),
+  "Helvetica Neue UltraLight Italic": require("../assets/fonts/helveticaNeue/HelveticaNeueUltraLightItalic.otf")
+};
+
+export const FONTS = { ...SOPHIA_PRO, ...HELVETICA_NEUE };
--- app/client/mobile/tailwind.config.js
+++ app/client/mobile/tailwind.config.js
@@ -1,31 +1,50 @@
 /** @type {import('tailwindcss').Config} */
 import { colors } from "./constants/colors";
 
+const PRIMARY = {
+  sans: ["Sofia Pro Regular", "sans-serif"],
+  bold: ["Sofia Pro Bold", "sans-serif"],
+  light: ["Sofia Pro Light", "sans-serif"],
+  medium: ["Sofia Pro Medium", "sans-serif"],
+  semibold: ["Sofia Pro Semi Bold", "sans-serif"],
+  black: ["Sofia Pro Black", "sans-serif"],
+  extralight: ["Sofia Pro ExtraLight", "sans-serif"],
+  ultralight: ["Sofia Pro UltraLight", "sans-serif"],
+  italic: ["Sofia Pro Regular Italic", "sans-serif"],
+  "bold-italic": ["Sofia Pro Bold Italic", "sans-serif"],
+  "light-italic": ["Sofia Pro Light Italic", "sans-serif"],
+  "medium-italic": ["Sofia Pro Medium Italic", "sans-serif"],
+  "semibold-italic": ["Sofia Pro Semi Bold Italic", "sans-serif"],
+  "black-italic": ["Sofia Pro Black Italic", "sans-serif"],
+  "extralight-italic": ["Sofia Pro ExtraLight Italic", "sans-serif"],
+  "ultralight-italic": ["Sofia Pro UltraLight Italic", "sans-serif"]
+};
+
+const SECONDARY = {
+  "secondary-black": ["Helvetica Neue Black", "sans-serif"],
+  "secondary-black-italic": ["Helvetica Neue Black Italic", "sans-serif"],
+  "secondary-bold": ["Helvetica Neue Bold", "sans-serif"],
+  "secondary-bold-italic": ["Helvetica Neue Bold Italic", "sans-serif"],
+  "secondary-heavy": ["Helvetica Neue Heavy", "sans-serif"],
+  "secondary-heavy-italic": ["Helvetica Neue Heavy Italic", "sans-serif"],
+  "secondary-italic": ["Helvetica Neue Italic", "sans-serif"],
+  "secondary-light": ["Helvetica Neue Light", "sans-serif"],
+  "secondary-light-italic": ["Helvetica Neue Light Italic", "sans-serif"],
+  "secondary-medium": ["Helvetica Neue Medium", "sans-serif"],
+  "secondary-medium-italic": ["Helvetica Neue Medium Italic", "sans-serif"],
+  "secondary-semibold": ["Helvetica Neue Semi Bold", "sans-serif"],
+  "secondary-roman": ["Helvetica Neue Roman", "sans-serif"],
+  "secondary-thin": ["Helvetica Neue Thin", "sans-serif"],
+  "secondary-thin-italic": ["Helvetica Neue Thin Italic", "sans-serif"],
+  "secondary-ultralight": ["Helvetica Neue UltraLight", "sans-serif"],
+  "secondary-ultralight-italic": ["Helvetica Neue UltraLight Italic", "sans-serif"]
+};
+
+const fontFamily = { ...PRIMARY, ...SECONDARY };
+
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
-  theme: {
-    extend: {
-      colors,
-      fontFamily: {
-        sans: ["Sofia-Pro-Regular", "sans-serif"],
-        bold: ["Sofia-Pro-Bold", "sans-serif"],
-        light: ["Sofia-Pro-Light", "sans-serif"],
-        medium: ["Sofia-Pro-Medium", "sans-serif"],
-        semibold: ["Sofia-Pro-Semi-Bold", "sans-serif"],
-        black: ["Sofia-Pro-Black", "sans-serif"],
-        extralight: ["Sofia-Pro-ExtraLight", "sans-serif"],
-        ultralight: ["Sofia-Pro-UltraLight", "sans-serif"],
-        italic: ["Sofia-Pro-Regular-Italic", "sans-serif"],
-        "bold-italic": ["Sofia-Pro-Bold-Italic", "sans-serif"],
-        "light-italic": ["Sofia-Pro-Light-Italic", "sans-serif"],
-        "medium-italic": ["Sofia-Pro-Medium-Italic", "sans-serif"],
-        "semibold-italic": ["Sofia-Pro-Semi-Bold-Italic", "sans-serif"],
-        "black-italic": ["Sofia-Pro-Black-Italic", "sans-serif"],
-        "extralight-italic": ["Sofia-Pro-ExtraLight-Italic", "sans-serif"],
-        "ultralight-italic": ["Sofia-Pro-UltraLight-Italic", "sans-serif"]
-      }
-    }
-  },
+  theme: { extend: { colors, fontFamily }},
   plugins: []
 };
