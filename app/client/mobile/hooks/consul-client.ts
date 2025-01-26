4b6d458573023ebea752c53554830343c47a3690
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2025-01-26 22:23:30 +0700

    fe: update env variable

--- app/client/mobile/hooks/consul-client.ts
+++ app/client/mobile/hooks/consul-client.ts
@@ -34,37 +34,43 @@ type Props = {
 
 /**
  * get the service uri from service discovery by selecting the first instance
+ * 
  * TODO: implement round-robin if there are 2 or more instances
- * @param serviceName
- * @returns
+ * @param serviceName 
+ * @returns service uri or undefined
  */
+const fetchServiceUri = async (serviceName: string) => {
+  const response = await fetch(
+    `${CONSUL_BASE_URL}/v1/health/service/${serviceName}?passing=true`
+  );
+  const services: ConsulServiceResponse[] = await response.json();
+  if (services.length == 0) {
+    console.error(`No healthy instances found for service: ${serviceName}`);
+    return;
+  }
+  const service = services[0].Service;
+  const address = service.Address || services[0].Node.Address;
+  const port = service.Port;
+  const isSecure = service.Tags.includes("secure=true");
+  const scheme = isSecure ? "https" : "http";
+  const uri = transformPlatformURI(`${scheme}://${address}:${port}`);
+
+  console.log("Service query successful with URI", uri);
+  return uri;
+};
+
 const useServiceDiscovery = ({ serviceName }: Props): string => {
   const [serviceUri, setServiceUri] = useState<string>("");
-  
-  useEffect(() => {
-    const fetchServiceUri = async () => {
-      const response = await fetch(
-        `${CONSUL_BASE_URL}/v1/health/service/${serviceName}?passing=true`
-      );
-      const services: ConsulServiceResponse[] = await response.json();
-      if (services.length == 0) {
-        console.error(`No healthy instances found for service: ${serviceName}`);
-        return;
-      }
-      const service = services[0].Service;
-      const address = service.Address || services[0].Node.Address;
-      const port = service.Port;
-      const isSecure = service.Tags.includes("secure=true");
-      const scheme = isSecure ? "https" : "http";
-      const uri = transformPlatformURI(`${scheme}://${address}:${port}`);
 
-      setServiceUri(uri);
-      console.log("Service query successful with URI", uri);
+  useEffect(() => {
+    const getServiceUri = async () => {
+      const uri = await fetchServiceUri(serviceName);
+      if (uri) setServiceUri(uri);
     };
-    fetchServiceUri();
+    getServiceUri();
   }, []);
 
   return serviceUri;
 };
 
-export default useServiceDiscovery;
+export { useServiceDiscovery, fetchServiceUri };
