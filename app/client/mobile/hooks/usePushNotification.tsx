d74c59b7dc1d2d9363581843019c360180e275b3
Phen Chua <89496400+PhenChua29@users.noreply.github.com>
2025-01-31 04:51:22 +0700

    fe: fix notification hook

--- app/client/mobile/hooks/usePushNotification.tsx
+++ app/client/mobile/hooks/usePushNotification.tsx
@@ -1,4 +1,3 @@
-import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
 import Constants from "expo-constants";
 import { Alert, Platform } from "react-native";
@@ -30,36 +29,33 @@ export const usePushNotification = (): PushNotificationState => {
 
   const registerForPushNotificationAsync = async () => {
     let token;
-    if (Device.isDevice) {
-      const { status: existingStatus } = await Notifications.getPermissionsAsync();
-      let finalStatus = existingStatus;
-      // Request permission again if the status is not granted
-      if (existingStatus !== Notifications.PermissionStatus.GRANTED) {
-        const { status } = await Notifications.getPermissionsAsync();
-        finalStatus = existingStatus;
-      }
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
 
-      if (finalStatus !== Notifications.PermissionStatus.GRANTED) {
-        Alert.alert("In order to have notification, you have to allowed this permission");
-        return;
-      }
-
-      token = await Notifications.getExpoPushTokenAsync({
-        projectId: Constants.expoConfig?.extra?.eas?.projectId
-      });
-
-      if (Platform.OS === "android") {
-        Notifications.setNotificationChannelAsync("default", {
-          name: "default",
-          importance: Notifications.AndroidImportance.MAX,
-          vibrationPattern: [0, 250, 250, 250],
-          lightColor: "#FF231F7C"
-        });
-      }
-      return token;
-    } else {
-      console.error("Error: Please use physical device");
+    // Request permission again if the status is not granted
+    if (existingStatus !== Notifications.PermissionStatus.GRANTED) {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
     }
+
+    if (finalStatus !== Notifications.PermissionStatus.GRANTED) {
+      Alert.alert("In order to have notification, you have to allowed this permission");
+      return;
+    }
+
+    token = await Notifications.getExpoPushTokenAsync({
+      projectId: Constants.expoConfig?.extra?.eas?.projectId
+    });
+
+    if (Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C"
+      });
+    }
+    return token;
   };
   useEffect(() => {
     registerForPushNotificationAsync().then(token => {
