b795e096db6959b766b8db2e307254c154dcbb13
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2025-01-31 23:52:52 +0700

    fe: update usePushNotification hook

--- app/client/mobile/hooks/usePushNotification.tsx
+++ app/client/mobile/hooks/usePushNotification.tsx
@@ -2,6 +2,7 @@ import * as Notifications from "expo-notifications";
 import Constants from "expo-constants";
 import { Alert, Platform } from "react-native";
 import { useEffect, useRef, useState } from "react";
+import { protectedAxiosInstance } from "@/constants/host";
 
 export interface PushNotificationState {
   notification?: Notifications.Notification;
@@ -37,15 +38,22 @@ export const usePushNotification = (): PushNotificationState => {
       const { status } = await Notifications.requestPermissionsAsync();
       finalStatus = status;
     }
+    console.log({ finalStatus });
 
     if (finalStatus !== Notifications.PermissionStatus.GRANTED) {
       Alert.alert("In order to have notification, you have to allowed this permission");
+      console.log("1");
+
       return;
     }
+    console.log("2");
+    console.log(Constants.expoConfig?.extra?.eas?.projectId);
 
     token = await Notifications.getExpoPushTokenAsync({
       projectId: Constants.expoConfig?.extra?.eas?.projectId
     });
+    console.log("Token here");
+    console.log({ token });
 
     if (Platform.OS === "android") {
       Notifications.setNotificationChannelAsync("default", {
@@ -54,10 +62,22 @@ export const usePushNotification = (): PushNotificationState => {
         vibrationPattern: [0, 250, 250, 250],
         lightColor: "#FF231F7C"
       });
+      try {
+        await protectedAxiosInstance.post("api/notification/expo-push-token/android", {
+          expoPushToken
+        });
+        console.log("Register user with push token successfully");
+      } catch (err) {
+        console.error(
+          `Register user with expo push token ${expoPushToken} failed, Please try again`
+        );
+        console.error(err);
+      }
     }
     return token;
   };
   useEffect(() => {
+    console.log("register notification");
     registerForPushNotificationAsync().then(token => {
       setExpoPushToken(token);
     });
@@ -78,6 +98,7 @@ export const usePushNotification = (): PushNotificationState => {
       Notifications.removeNotificationSubscription(responseListener.current!);
     };
   }, []);
+
   return {
     expoPushToken,
     notification
