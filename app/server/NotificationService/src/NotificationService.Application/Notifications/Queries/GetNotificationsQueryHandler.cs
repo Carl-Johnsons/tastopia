e745ec71174bfb3c5b43cc70981d70b89d38a83d
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2025-02-04 18:20:35 +0700

    be: add get notification query handler

--- app/server/NotificationService/src/NotificationService.Application/Notifications/Queries/GetNotificationsQueryHandler.cs
+++ app/server/NotificationService/src/NotificationService.Application/Notifications/Queries/GetNotificationsQueryHandler.cs
@@ -1,67 +1,114 @@
-﻿
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization;
+﻿using AutoMapper;
+using Contract.DTOs.UserDTO;
+using Google.Protobuf.Collections;
 using MongoDB.Driver;
-using NotificationService.Application.Responses;
-using System.Text.RegularExpressions;
+using NotificationService.Domain.Errors;
+using NotificationService.Domain.Responses;
+using SmartFormat;
 using UserProto;
 
 namespace NotificationService.Application.Notifications.Queries;
 
-public record GetNotificationsQuery : IRequest<Result<List<NotificationsResponse>>>
+public record GetNotificationsQuery : IRequest<Result<PaginatedNotificationListResponse>>
 {
     public Guid AccountId { get; set; }
     public string Language { get; init; } = "en";
 }
 
-public class GetNotificationsQueryHandler : IRequestHandler<GetNotificationsQuery, Result<List<NotificationsResponse>>>
+public partial class GetNotificationsQueryHandler : IRequestHandler<GetNotificationsQuery, Result<PaginatedNotificationListResponse>>
 {
     private readonly IApplicationDbContext _context;
-    private readonly GrpcUser.GrpcUserClient _userClient;
+    private readonly GrpcUser.GrpcUserClient _grpcUserClient;
+    private readonly IMapper _mapper;
+    private readonly IPaginateDataUtility<NotificationsResponse, AdvancePaginatedMetadata> _paginateDataUtility;
 
-    public GetNotificationsQueryHandler(IApplicationDbContext context, GrpcUser.GrpcUserClient userClient)
+    public GetNotificationsQueryHandler(IApplicationDbContext context, GrpcUser.GrpcUserClient grpcUserClient, IMapper mapper, IPaginateDataUtility<NotificationsResponse, AdvancePaginatedMetadata> paginateDataUtility)
     {
         _context = context;
-        _userClient = userClient;
+        _grpcUserClient = grpcUserClient;
+        _mapper = mapper;
+        _paginateDataUtility = paginateDataUtility;
     }
 
-    public Task<Result<List<NotificationsResponse>>> Handle(GetNotificationsQuery request, CancellationToken cancellationToken)
+    public async Task<Result<PaginatedNotificationListResponse>> Handle(GetNotificationsQuery request, CancellationToken cancellationToken)
     {
         var database = _context.GetDatabase();
-        var notificationsCollection = database.GetCollection<Notification>("Notification");
+        var nQuery = database.GetCollection<Notification>(nameof(Notification))
+                             .AsQueryable()
+                             .AsQueryable();
+        var ntQuery = database.GetCollection<NotificationTemplate>(nameof(NotificationTemplate))
+                              .AsQueryable()
+                              .AsQueryable();
 
-        var pipeline = new[]
+        var notificationQuery = nQuery.Join(
+                inner: ntQuery,
+                outerKeySelector: n => n.TemplateId,
+                innerKeySelector: nt => nt.Id,
+                resultSelector: (n, nt) => new Notification
+                {
+                    Id = n.Id,
+                    PrimaryActors = n.PrimaryActors,
+                    SecondaryActors = n.SecondaryActors,
+                    TemplateId = n.TemplateId,
+                    CreatedAt = n.CreatedAt,
+                    ImageUrl = n.ImageUrl,
+                    JsonData = n.JsonData,
+                    UpdatedAt = n.UpdatedAt,
+                    Template = nt
+                }
+            );
+
+        Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(notificationQuery.ToList(), Newtonsoft.Json.Formatting.Indented));
+
+        var actorIdSets = notificationQuery.SelectMany(n => n.PrimaryActors.Concat(n.SecondaryActors)
+                                                                  .Select(merge => merge.ActorId.ToString()))
+                                  .ToHashSet();
+
+        Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(actorIdSets, Newtonsoft.Json.Formatting.Indented));
+        var res = await _grpcUserClient.GetSimpleUserAsync(new GrpcGetSimpleUsersRequest
         {
-            new BsonDocument("$lookup", new BsonDocument
+            AccountId = { _mapper.Map<RepeatedField<string>>(actorIdSets) }
+        }, cancellationToken: cancellationToken);
+
+        var mapUsers = new Dictionary<Guid, SimpleUser>();
+        foreach (var (key, value) in res.Users)
+        {
+            mapUsers[Guid.Parse(key)] = new SimpleUser
             {
-                { "from", "NotificationTemplate" },
-                { "localField", "TemplateId" },
-                { "foreignField", "_id" },
-                { "as", "Template" }
-            }),
-            new BsonDocument("$unwind", "$Template")
-        };
-
-        var result = notificationsCollection.Aggregate<BsonDocument>(pipeline).ToList();
-
-        foreach (var doc in result)
-        {
-            Console.WriteLine(doc.ToJson());
+                AccountId = Guid.Parse(value.AccountId),
+                AvtUrl = value.AvtUrl,
+                DisplayName = value.DisplayName,
+            };
         }
-
-        var responses = result.Select(r => BsonSerializer.Deserialize<Notification>(r))
+        if (res == null || mapUsers.Count != actorIdSets.Count)
+        {
+            return Result<PaginatedNotificationListResponse>.Failure(NotificationErrors.NotFound, "Actor not found");
+        }
+        var responses = notificationQuery
+                                .ToList()
                                 .Select(n =>
                                 {
-                                    var paIds = (n?.PrimaryActors ?? []).Select(pa => pa.ActorId).ToList();
-                                    var saIds = (n?.SecondaryActors ?? []).Select(pa => pa.ActorId).ToList();
+                                    if (n == null)
+                                    {
+                                        return null;
+                                    }
+                                    var paNames = n.PrimaryActors.Select(pa => mapUsers[pa.ActorId].DisplayName).ToList();
+                                    var saNames = n.SecondaryActors.Select(sa => mapUsers[sa.ActorId].DisplayName).ToList();
 
-                                    var message = n?.Template?.TranslationMessages.GetValueOrDefault(request.Language)
-                                                  ?? "Fallback message";
-                                    var title = n?.Template?.TranslationTitles?.GetValueOrDefault(request.Language)
-                                                ?? "Fallback title";
+                                    var message = n.Template?.TranslationMessages.GetValueOrDefault(request.Language)
+                                                  ?? "";
+                                    var title = n.Template?.TranslationTitles?.GetValueOrDefault(request.Language)
+                                                ?? "";
 
-                                    message = SafeFormat(message, string.Join(", ", paIds), string.Join(", ", saIds));
-                                    title = SafeFormat(title, string.Join(", ", paIds), string.Join(", ", saIds));
+                                    var data = new
+                                    {
+                                        Actors = paNames,
+                                        Targets = saNames,
+                                        IsSelf = false
+                                    };
+
+                                    message = Smart.Format(message, data);
+                                    title = Smart.Format(title);
 
                                     return new NotificationsResponse
                                     {
@@ -76,30 +123,17 @@ public class GetNotificationsQueryHandler : IRequestHandler<GetNotificationsQuer
                                 })
                                 .ToList();
 
-        return Task.FromResult(Result<List<NotificationsResponse>>.Success(responses));
-    }
-
-    private string SafeFormat(string template, params object[] args)
-    {
-        if (string.IsNullOrEmpty(template))
-            return template;
-
-        // Regular expression to find placeholders in the format {n}
-        var regex = new Regex(@"\{(\d+)\}");
-        return regex.Replace(template, match =>
+        var paginatedResponse = new PaginatedNotificationListResponse
         {
-            // Extract the index from the placeholder
-            if (int.TryParse(match.Groups[1].Value, out int index))
+            PaginatedData = responses!,
+            Metadata = new AdvancePaginatedMetadata
             {
-                // Check if the index is within the bounds of the arguments array
-                if (index >= 0 && index < args.Length)
-                {
-                    // Return the argument's string representation
-                    return args[index]?.ToString() ?? string.Empty;
-                }
+                HasNextPage = true,
+                TotalPage = 1
             }
-            // If no corresponding argument is found, return an empty string or a default value
-            return string.Empty; // Or use a default value like "[Missing]"
-        });
+        };
+
+        return Result<PaginatedNotificationListResponse>.Success(paginatedResponse);
     }
+
 }
