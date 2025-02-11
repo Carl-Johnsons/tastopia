4caa9e0ffbdfe45c57b8f60ce179c087af895975
Ba Thinh <thefanofanimeromanceandcomedy@gmail.com>
2025-02-11 17:03:40 +0700

    be: add get user followers api

---
+++ app/server/UserService/src/UserService.Domain/Responses/FollowUser.response.cs
@@ -0,0 +1,8 @@
+﻿namespace UserService.Domain.Responses;
+
+public class FollowUserResponse
+{
+    public Guid FollowerId { get; set; }
+    public Guid FollowingId { get; set; }
+    public bool IsFollowing { get; set; }
+}
