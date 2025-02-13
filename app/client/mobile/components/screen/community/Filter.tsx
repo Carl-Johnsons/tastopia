7f89b5af639064ae824cde98298e892621a8200f
Quoc Vuong <quocvuongle.ct@gmail.com>
2025-02-13 09:10:35 +0700

    fe: update filter shadow

--- app/client/mobile/components/screen/community/Filter.tsx
+++ app/client/mobile/components/screen/community/Filter.tsx
@@ -44,20 +44,25 @@ const Filter = ({ filterSelected, handleSelect }: FilterProps) => {
           >
             <View
               className={`flex-center flex-row gap-2 rounded-full border-2 border-primary px-1 py-1 ${isSelected ? "bg-primary" : "bg-white_black"}`}
-              style={Platform.select({
-                ios: {
-                  shadowColor: "#000",
-                  shadowOffset: { width: 0, height: 2 },
-                  shadowOpacity: 0.2,
-                  shadowRadius: 30
-                },
-                android: { elevation: 5 }
-              })}
             >
-              <View className={`rounded-full p-[0.5px] ${isSelected ? "bg-white" : ""}`}>
+              <View
+                className={`rounded-full p-[0.5px] ${isSelected ? "bg-white" : ""}`}
+                style={
+                  !isSelected &&
+                  Platform.select({
+                    ios: {
+                      shadowColor: "#000",
+                      shadowOffset: { width: 0, height: 2 },
+                      shadowOpacity: 0.3,
+                      shadowRadius: 10
+                    },
+                    android: { elevation: 5 }
+                  })
+                }
+              >
                 <Image
                   source={item.imageUrl}
-                  style={{ width: 50, height: 50, borderRadius: 50 }}
+                  style={[{ width: 50, height: 50, borderRadius: 50 }]}
                   cachePolicy={"disk"}
                 />
               </View>
