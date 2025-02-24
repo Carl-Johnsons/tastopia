5dbbba1148dac3d85e61403cdcda7b77a761cd5b
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2025-02-24 19:45:03 +0700

    be: add unread notification count

---
+++ app/server/NotificationService/src/NotificationService.Domain/Responses/Paginated.metadata.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json;
+
+namespace NotificationService.Domain.Responses;
+
+public class NotificationListMetadata : AdvancePaginatedMetadata
+{
+    [JsonProperty("unreadNotifications")]
+    public int UnreadNotifications { get; set; }
+}
