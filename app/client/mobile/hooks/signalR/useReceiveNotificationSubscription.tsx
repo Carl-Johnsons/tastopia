d78d4bedee608784fea9f54f7e33046c12a7b584
Phen Chua <89496400+PhenChua29@users.noreply.github.com>
2025-02-24 20:14:47 +0700

    fe: invalidate query on socket disconnect

--- app/client/mobile/hooks/signalR/useReceiveNotificationSubscription.tsx
+++ app/client/mobile/hooks/signalR/useReceiveNotificationSubscription.tsx
@@ -1,16 +1,20 @@
 import { useCallback } from "react";
 import { HubConnection } from "@microsoft/signalr";
 import { SignalREvent } from "@/constants/signalr";
+import { useQueryClient } from "react-query";
 
 const useReceiveNotificationSubscription = () => {
+  const queryClient = useQueryClient();
   const subscribeReceiveNotificationEvent = useCallback((connection?: HubConnection) => {
     if (!connection) {
       return;
     }
     connection.on(SignalREvent.RECEIVE_NOTIFICATION, () => {
       if (!connection) {
+        queryClient.invalidateQueries("getNotification");
         return;
       }
+
       console.log("Receive notification");
     });
   }, []);
