3f3e11e9608e8024ea3fc8d151470b691d483e09
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2025-02-24 15:53:35 +0700

    be: notify user with specify user setting

--- app/server/NotificationService/src/NotificationService.Application/Notifications/Commands/NotifyUserCommandHandler.cs
+++ app/server/NotificationService/src/NotificationService.Application/Notifications/Commands/NotifyUserCommandHandler.cs
@@ -48,6 +48,13 @@ public class NotifyUserCommandHandler : IRequestHandler<NotifyUserCommand, Resul
         _mapper = mapper;
     }
 
+    private record UserSettingObj
+    {
+        public string Code { get; set; } = null!;
+        public string Value { get; set; } = null!;
+        public SettingDataType DataType { get; set; }
+    }
+
     public async Task<Result> Handle(NotifyUserCommand request,
                                CancellationToken cancellationToken)
     {
@@ -128,25 +135,77 @@ public class NotifyUserCommandHandler : IRequestHandler<NotifyUserCommand, Resul
                 AccountId = { _mapper.Map<RepeatedField<string>>(recipientIdSet) }
             }, cancellationToken: cancellationToken);
 
-            var mapUserLanguageSettings = new Dictionary<Guid, string>();
+            var mapUserSettings = new Dictionary<Guid, List<UserSettingObj>>();
 
             foreach (var (key, value) in settingRes.SettingMap)
             {
-                var languageSetting = LanguageUtility.ToIso6391(value.Settings.SingleOrDefault(s => s.SettingCode == "LANGUAGE")?.SettingValue
-                                                                ?? "en");
-                mapUserLanguageSettings[Guid.Parse(key)] = languageSetting;
+                List<UserSettingObj> userSetting = [];
+                foreach (var setting in value.Settings)
+                {
+                    if (setting.SettingCode == SETTING_KEY.LANGUAGE.ToString())
+                    {
+                        var languageSetting = LanguageUtility.ToIso6391(value.Settings.SingleOrDefault(s => s.SettingCode == SETTING_KEY.LANGUAGE.ToString())?.SettingValue
+                                                    ?? "en");
+                        userSetting.Add(new UserSettingObj
+                        {
+                            Code = SETTING_KEY.LANGUAGE.ToString(),
+                            Value = languageSetting,
+                            DataType = (SettingDataType)Enum.Parse(typeof(SettingDataType), setting.SettingType)
+                        });
+                        continue;
+                    }
+
+                    userSetting.Add(new UserSettingObj
+                    {
+                        Code = setting.SettingCode,
+                        Value = setting.SettingValue,
+                        DataType = (SettingDataType)Enum.Parse(typeof(SettingDataType), setting.SettingType)
+                    });
+                }
+
+                mapUserSettings[Guid.Parse(key)] = userSetting;
             }
 
-            Console.WriteLine(JsonConvert.SerializeObject(mapUserLanguageSettings, Formatting.Indented));
+            var mapSettingNotificationTemplate = new Dictionary<string, List<string>> {
+                {SETTING_KEY.NOTIFICATION_FOLLOW.ToString(), [NotificationTemplateCode.USER_FOLLOW.ToString()]},
+                {SETTING_KEY.NOTIFICATION_VOTE.ToString(), [NotificationTemplateCode.USER_DOWNVOTE.ToString(),
+                                                            NotificationTemplateCode.USER_UPVOTE.ToString()]},
+                {SETTING_KEY.NOTIFICATION_COMMENT.ToString(), [NotificationTemplateCode.USER_COMMENT.ToString()]},
+            };
 
-            foreach (var (key, languageCode) in mapUserLanguageSettings)
+
+            foreach (var (key, value) in mapUserSettings)
             {
                 var paNames = notification.PrimaryActors.Select(pa => mapUsers[pa.ActorId].DisplayName).ToList();
                 var saNames = notification.SecondaryActors.Select(sa => mapUsers[sa.ActorId].DisplayName).ToList();
-                var message = template.TranslationMessages.GetValueOrDefault(languageCode)
+                var message = template.TranslationMessages.GetValueOrDefault(value.SingleOrDefault(v => v.Code
+                                                                                                        == SETTING_KEY.LANGUAGE.ToString())!.Value)
                               ?? "";
-                var title = notification.Template?.TranslationTitles?.GetValueOrDefault(languageCode)
+                var title = notification.Template?.TranslationTitles?.GetValueOrDefault(value.SingleOrDefault(v => v.Code
+                                                                                                                   == SETTING_KEY.LANGUAGE.ToString())!.Value)
                             ?? "";
+
+                bool shouldNotify = true;
+
+                foreach (var setting in value)
+                {
+                    if (setting.DataType.ToString() == SettingDataType.Boolean.ToString()
+                        && mapSettingNotificationTemplate.ContainsKey(setting.Code)
+                        && mapSettingNotificationTemplate[setting.Code].Contains(request.TemplateCode.ToString())
+                        && !Boolean.Parse(value.SingleOrDefault(v => v.Code == setting.Code)!.Value))
+                    {
+                        shouldNotify = false;
+                        break;
+                    }
+                }
+
+
+                if (!shouldNotify)
+                {
+                    _logger.LogInformation($"Should notify {shouldNotify} with template {request.TemplateCode.ToString()}");
+                    continue;
+                }
+
                 var data = new
                 {
                     Actors = paNames,
@@ -170,7 +229,6 @@ public class NotifyUserCommandHandler : IRequestHandler<NotifyUserCommand, Resul
                         Title = title,
                     });
                 }
-
             }
         }
 
