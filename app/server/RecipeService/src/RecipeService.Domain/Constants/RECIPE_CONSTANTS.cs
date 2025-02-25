46ca6d0086434a6fe46c3bdbcf858c5a852188b5
Ba Thinh <thefanofanimeromanceandcomedy@gmail.com>
2025-02-25 17:12:00 +0700

    be: change admin get recipe limit to 6

--- app/server/RecipeService/src/RecipeService.Domain/Constants/RECIPE_CONSTANTS.cs
+++ app/server/RecipeService/src/RecipeService.Domain/Constants/RECIPE_CONSTANTS.cs
@@ -3,7 +3,7 @@
 public class RECIPE_CONSTANTS
 {
     public readonly static int RECIPE_LIMIT = 10;
-    public readonly static int ADMIN_RECIPE_LIMIT = 10;
+    public readonly static int ADMIN_RECIPE_LIMIT = 6;
     public readonly static int COMMENT_LIMIT = 10;
 
 }
