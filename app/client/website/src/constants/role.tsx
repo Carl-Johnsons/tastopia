5358336dbca76e43ee1a3680e6923d7308124613
Quoc Vuong <quocvuongle.ct@gmail.com>
2025-03-06 13:43:41 +0700

    fe: setup role and redux

---
+++ app/client/website/src/constants/role.tsx
@@ -0,0 +1,4 @@
+export enum Roles {
+  ADMIN,
+  SUPER_ADMIN,
+}
---
+++ app/client/website/src/hooks/auth/withAuth.tsx
@@ -0,0 +1,24 @@
+import { useRouter } from "next/router";
+import { Roles } from "@/constants/role";
+import { useEffect, ComponentType } from "react";
+import { useSelectRole } from "@/slices/auth.slice";
+
+const withAuth = <P extends object>(WrappedComponent: ComponentType<P>, allowedRoles: Roles[]) => {
+  const ProtectedComponent = (props: P) => {
+    const role = useSelectRole();
+
+    const router = useRouter();
+
+    useEffect(() => {
+      if (!role || !allowedRoles.includes(role)) {
+        router.replace("/unauthorized");
+      }
+    }, [role, router]);
+
+    return role && allowedRoles.includes(role) ? <WrappedComponent {...props} /> : null;
+  };
+
+  return ProtectedComponent;
+};
+
+export default withAuth;
