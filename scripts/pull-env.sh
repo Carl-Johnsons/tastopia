a0955f67ef3671d120ed56593ae0818f51fb2da7
Phen Chua <89496400+PhenChua29@users.noreply.github.com>
2025-03-07 16:58:42 +0700

    fe: update script

--- scripts/pull-env.sh
+++ scripts/pull-env.sh
@@ -40,4 +40,5 @@ pull_both_env_file "./" global_production &&
   pull_both_env_file "./app/server/TrackingService" tracking &&
   pull_both_env_file "./app/server/SubscriptionService" "subscription" &&
   pull_both_env_file "./app/server/IngredientPredictService" "ingredient-predict" &&
-  pull_both_env_file "./app/client/mobile" "mobile"
+  pull_both_env_file "./app/client/mobile" "mobile" &&
+  pull_both_env_file "./app/client/website" "website"
