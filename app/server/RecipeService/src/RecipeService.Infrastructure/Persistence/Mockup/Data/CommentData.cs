87673564c8cce6a41459fcbfdbe6cfadd14220e8
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2025-03-13 05:48:41 +0700

    be: migrate recipe data to json file

--- app/server/RecipeService/src/RecipeService.Infrastructure/Persistence/Mockup/Data/CommentData.cs
+++ app/server/RecipeService/src/RecipeService.Infrastructure/Persistence/Mockup/Data/CommentData.cs
@@ -1,32 +1,18 @@
-﻿using RecipeService.Domain.Entities;
+﻿using Newtonsoft.Json;
+using RecipeService.Domain.Entities;
 
 namespace RecipeService.Infrastructure.Persistence.Mockup.Data;
 
 public class CommentData
 {
-    private static readonly List<string> Comments = new List<string>
+    private static List<string> Comments = [];
+    private static readonly string SeedDataPath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.Parent?.Parent?.FullName!, "seeds") ?? "";
+
+    static CommentData()
     {
-        "This recipe looks amazing! Can't wait to try it.",
-        "I tried this yesterday, and it turned out perfect. Thanks for sharing!",
-        "This is such a creative idea. Love it!",
-        "The instructions are so clear and easy to follow. Great job!",
-        "Can I make this ahead of time?",
-        "I'm excited to try this recipe—it looks delicious!",
-        "This reminds me of a dish my grandmother used to make. Thank you!",
-        "Looks delicious! Any tips for making it spicier?",
-        "This was a hit with my family! Thank you for the recipe.",
-        "Perfect for the weekend! Can't wait to serve it to my guests.",
-        "I love how simple and easy this is. Great for busy days!",
-        "The combination of flavors in this recipe is fantastic!",
-        "This is going to be my go-to dish for potlucks. Thanks!",
-        "Such a comforting and hearty meal. Exactly what I needed!",
-        "I appreciate the detailed steps—it made cooking this so much easier.",
-        "The presentation in the picture is stunning! Inspiring to try plating like this.",
-        "I've been looking for a recipe like this for ages. Thank you!",
-        "My kids absolutely loved this dish. It's going into our regular rotation!",
-        "Such a unique recipe! It was a fun challenge to make.",
-        "This recipe is so versatile. I can see myself using it in many variations!"
-    };
+        var seedCommentFile = File.ReadAllText(Path.Combine(SeedDataPath, "comments.json"));
+        Comments = JsonConvert.DeserializeObject<List<string>>(seedCommentFile) ?? [];
+    }
 
     public static string GetRandomCommentContent()
     {
