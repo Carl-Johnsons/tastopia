9288508ba445c030f2e387c90e0418ac973b9978
Phen Chua <89496400+PhenChua29@users.noreply.github.com>
2025-03-14 19:00:40 +0700

    fe: move server actions to the proper file

--- app/client/website/src/actions/user.action.ts
+++ app/client/website/src/actions/user.action.ts
@@ -1,6 +1,11 @@
 "use server";
 
 import { protectedAxiosInstance } from "@/constants/host";
+import { SETTING_KEY, SETTING_VALUE } from "@/constants/settings";
+import { AxiosError } from "axios";
+import { UserState } from "@/slices/user.slice";
+import { IErrorResponseDTO } from "../../generated/interfaces/common.interface";
+import { stringify } from "@/utils/debug";
 
 export async function getUserById(id: string) {
   try {
@@ -31,3 +36,49 @@ export async function getUserActivitiesById(id: string, language: string) {
     throw error;
   }
 }
+
+export type UpdateSettingResponseSuccess = 0;
+export type UpdateSettingResponse = UpdateSettingResponseSuccess | IErrorResponseDTO;
+export type UpdateSettingParams = {
+  settings: Array<{
+    key: SETTING_KEY;
+    value: SETTING_VALUE.BOOLEAN | SETTING_VALUE.LANGUAGE;
+  }>;
+};
+
+export const useUpdateSettings = async (data: UpdateSettingParams) => {
+    const url = "/api/setting";
+
+    try {
+      const { data: response } =
+        await protectedAxiosInstance.put<UpdateSettingResponse>(url, data);
+      return response;
+    } catch (error) {
+      console.debug("useUpdateSettings", stringify(error));
+
+      if (error instanceof AxiosError) {
+        const data = error.response?.data as IErrorResponseDTO;
+        throw new Error(data.message);
+      }
+
+      throw new Error("An error has occurred.");
+    }
+};
+
+export type GetUserDetailsResponse = UserState;
+
+export const getUserDetails = async () => {
+  const url = "/api/user/get-current-user-details";
+
+  try {
+    const { data } = await protectedAxiosInstance.get(url);
+    return data as GetUserDetailsResponse;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      const data = error.response?.data as IErrorResponseDTO;
+      throw new Error(data.message ? data.message : error.message);
+    }
+
+    throw new Error("An error has occurred.");
+  }
+};
