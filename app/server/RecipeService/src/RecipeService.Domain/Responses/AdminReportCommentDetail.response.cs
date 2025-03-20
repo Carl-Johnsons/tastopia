869a19d42e13ca2a086bce1a587d14acff78ff53
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2025-03-20 23:32:52 +0700

    be: update comment detail response

--- app/server/RecipeService/src/RecipeService.Domain/Responses/AdminReportCommentDetail.response.cs
+++ app/server/RecipeService/src/RecipeService.Domain/Responses/AdminReportCommentDetail.response.cs
@@ -2,10 +2,18 @@
 
 public class AdminReportCommentDetailResponse
 {
-    public Guid CommentId { get; set; }
-    public string CommentOwnerUsername { get; set; } = null!;
-    public DateTime CreatedAt { get; set; }
-    public string CommentContent { get; set; } = null!;
+    public CommentDetailResponse Comment { get; set; } = null!;
     public AdminRecipeResponse Recipe { get; set; } = null!;
     public List<ReportRecipeResponse> Reports { get; set; } = [];
+}
+
+public class CommentDetailResponse
+{
+    public Guid Id { get; set; }
+    public Guid AuthorId { get; set; }
+    public string AuthorUsername { get; set; } = null!;
+    public string Content { get; set; } = null!;
+    public bool IsActive { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
 }
