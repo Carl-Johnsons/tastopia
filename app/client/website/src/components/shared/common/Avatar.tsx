c33411e317fe25fc457263073bfc14a7699b6c14
Phen Chua <89496400+PhenChua29@users.noreply.github.com>
2025-03-28 18:17:02 +0700

    fe: update profile ui

--- app/client/website/src/components/shared/common/Avatar.tsx
+++ app/client/website/src/components/shared/common/Avatar.tsx
@@ -3,15 +3,16 @@ import {
   AvatarImage,
   Avatar as BaseAvatar
 } from "@/components/ui/avatar";
+import { AvatarProps } from "@radix-ui/react-avatar";
 
 type Props = {
   src: string;
   alt: string;
-};
+} & Pick<AvatarProps, "className">;
 
-export default function Avatar({ src, alt }: Props) {
+export default function Avatar({ src, alt, className }: Props) {
   return (
-    <BaseAvatar className='min-w-fit cursor-pointer'>
+    <BaseAvatar className={`min-w-fit cursor-pointer ${className}`}>
       <AvatarImage
         className='bg-orange-100'
         src={src}
