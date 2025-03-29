872ebc5dd03a9e397c33388fd72ab6b5b262c5e5
Phen Chua <89496400+PhenChua29@users.noreply.github.com>
2025-03-29 17:15:38 +0700

    fe: refactor login/logout flow

--- app/client/website/src/components/screen/admins/ProfileHeader.tsx
+++ app/client/website/src/components/screen/admins/ProfileHeader.tsx
@@ -2,24 +2,21 @@
 
 import { useState } from "react";
 import { useTranslations } from "next-intl";
-import { DisableAdminButton, RestoreAdminButton, SignOutButton } from "./Button";
+import { SignOutButton } from "./Button";
 import { ItemStatusText } from "../report/common/StatusText";
 import { IAdminDetailResponse } from "@/generated/interfaces/user.interface";
 import { Roles } from "@/constants/role";
 import Avatar from "@/components/shared/common/Avatar";
-import { Button } from "@/components/ui/button";
-import { handleSignOut } from "@/actions/auth";
 
 type Props = {
   admin: IAdminDetailResponse;
 };
 
 export default function ProfileHeader({ admin }: Props) {
-  const { accountId, avatarUrl, username } = admin;
+  const { avatarUrl, username } = admin;
   const role = Roles.ADMIN;
-  const [isActive, setIsActive] = useState(admin.isActive);
+  const [isActive] = useState(admin.isActive);
   const tRole = useTranslations("administerAdmins.detail.header.roles");
-  const tTooltip = useTranslations("administerAdmins.tooltip");
 
   return (
     <div className='bg-white_black100 overflow-hidden rounded-xl border border-gray-200 shadow-sm dark:border-gray-600'>
