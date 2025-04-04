27bb239f05706aa485fffc47a68c004fcc87fe71
Phen Chua <89496400+PhenChua29@users.noreply.github.com>
2025-04-04 20:18:00 +0700

    fe: change data fetching routes

--- app/client/website/src/components/shared/auth/AuthListener.tsx
+++ app/client/website/src/components/shared/auth/AuthListener.tsx
@@ -5,44 +5,30 @@ import { useSession } from "next-auth/react";
 import { useAppDispatch } from "@/store/hooks";
 import { clearAuthData, saveAuthData } from "@/slices/auth.slice";
 import { clearUserData, saveUserData } from "@/slices/user.slice";
-import { useGetCurrentUser } from "@/api/user";
 import { jwtDecode } from "jwt-decode";
 import { Roles } from "@/constants/role";
+import { useGetCurrentAdminDetail } from "@/api/admin";
 
 const AuthListener = () => {
   const { data: session } = useSession();
   const dispatch = useAppDispatch();
-  const { refetch } = useGetCurrentUser();
+  const { data: user } = useGetCurrentAdminDetail();
 
   const clearData = useCallback(() => {
     dispatch(clearAuthData());
     dispatch(clearUserData());
   }, [dispatch]);
 
-  const fetchUserDetails = useCallback(async () => {
-    try {
-      const { data } = await refetch();
-      if (data) dispatch(saveUserData({ ...data }));
-    } catch (error) {
-      console.error(error);
-    }
-  }, [refetch, dispatch]);
-
   useEffect(() => {
     if (!session) return clearData();
+    if (user) dispatch(saveUserData({ ...user }));
 
-    fetchUserDetails();
     const accessToken = session.accessToken as string;
     const idToken = session.idToken as string;
     const decodedToken = jwtDecode(accessToken) as any;
 
     dispatch(saveAuthData({ accessToken, idToken, role: decodedToken.role as Roles }));
-
-    // clientAxiosInstance.post("/api/auth/cookie", {
-    // accessToken,
-    // idToken
-    // });
-  }, [session, dispatch, clearData, fetchUserDetails]);
+  }, [session, dispatch, clearData, user]);
 
   return null;
 };
