685085a7386ef41f393b6884dc3098c8c5e55128
Ba Thinh <thefanofanimeromanceandcomedy@gmail.com>
2025-04-10 12:15:55 +0700

    be: add get translated tag category in admin get tags api

--- app/server/RecipeService/src/RecipeService.Domain/Constants/TAG_CONSTANTS.cs
+++ app/server/RecipeService/src/RecipeService.Domain/Constants/TAG_CONSTANTS.cs
@@ -1,4 +1,6 @@
-﻿namespace RecipeService.Domain.Constants;
+﻿using Contract.Constants;
+
+namespace RecipeService.Domain.Constants;
 
 public class TAG_CONSTANTS
 {
@@ -6,4 +8,25 @@ public class TAG_CONSTANTS
     public readonly static int ADMIN_TAG_LIMIT = 6;
     public readonly static int TAG_RANKING_LIMIT = 10;
 
+    private readonly static Dictionary<TagCategory, string> TAG_CATEGORY_VI_STRING = new Dictionary<TagCategory, string>
+    {
+        { TagCategory.All, "Tất cả" },
+        { TagCategory.Ingredient, "Nguyên liệu" },
+        { TagCategory.DishType, "Loại món ăn" }
+    };
+
+    public static string GetTagCategoryTranslate(TagCategory category, string lang)
+    {
+        if(lang == LanguageValidation.En)
+        {
+            return category.ToString();
+        }
+
+        if (lang == LanguageValidation.Vi)
+        {
+            return TAG_CATEGORY_VI_STRING[category];
+        }
+
+        return "";
+    }
 }
