f3188c674481124d953e9bb9522ae51ef7c3ad98
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2025-10-16 16:06:26 +0700

    do: update setup-backend script

--- scripts/setup-backend.sh
+++ scripts/setup-backend.sh
@@ -3,25 +3,30 @@
 . ./scripts/lib.sh && check_docker
 
 dotnet tool install --global dotnet-ef --version 8.0.0
-[[ "$PLATFORM" != "windows" ]] && echo 'export PATH="$PATH:$HOME/.dotnet/tools"' >> ~/.bashrc && . ~/.bashrc
-echo "Install dotnet-ef successfully"
+if [[ "$PLATFORM" != "windows" ]]; then
+    echo 'export PATH="$PATH:$HOME/.dotnet/tools"' >> ~/.bashrc
+    . ~/.bashrc
+    printf "\n\t*** ${DEBUG}Add dotnet-ef to PATH and source ~/bashrc${NC} ***\n\n"
+fi
+
+printf "\n\t*** ${SUCCESS}INSTALL dotnet-ef SUCCESSFULLY${NC} ***\n\n"
 
 ./scripts/pull-env.sh
-printf "\n\t*** ${GREEN}DONE PULLING ENV${NC} ***\n\n"
+printf "\n\t*** ${SUCCESS}DONE PULLING ENV${NC} ***\n\n"
 
 run_required_docker_services
 
-[[ "$PLATFORM" != "windows" ]] && sudo chown $(whoami) data -R && echo -e "${GREEN}Run chown for data directory successfully${NC}"
-printf "\n\t*** ${GREEN}DONE RUNNING CONTAINER${NC} ***\n\n"
+[[ "$PLATFORM" != "windows" ]] && sudo chown $(whoami) data -R && echo -e "${SUCCESS}Run chown for data directory successfully${NC}"
+printf "\n\t*** ${SUCCESS}DONE RUNNING CONTAINER${NC} ***\n\n"
 
 ./scripts/build-all-services.sh
-printf "\n\t*** ${GREEN}DONE BUILDING ALL SERVICES${NC} ***\n\n"
+printf "\n\t*** ${SUCCESS}DONE BUILDING ALL SERVICES${NC} ***\n\n"
 
 ./scripts/apply-all-migrations.sh
-printf "\n\t*** ${GREEN}DONE APPLY ALL MIGRATIONS${NC} ***\n\n"
+printf "\n\t*** ${SUCCESS}DONE APPLY ALL MIGRATIONS${NC} ***\n\n"
 
 ./scripts/config-docker-compose.sh
-printf "\n\t*** ${GREEN}DONE GENERATING DOCKER COMPOSE OVERRIDE FILE${NC} ***\n\n"
+printf "\n\t*** ${SUCCESS}DONE GENERATING DOCKER COMPOSE OVERRIDE FILE${NC} ***\n\n"
 
 ./scripts/setup-capture-service.sh
-printf "\n\t*** ${GREEN}DONE SETUP CAPTURE SERVICE${NC} ***\n\n"
+printf "\n\t*** ${SUCCESS}DONE SETUP CAPTURE SERVICE${NC} ***\n\n"
