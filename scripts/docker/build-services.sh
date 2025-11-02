63537ad41a7af5eae43bcde24b02d98a0a11dd4c
Phen Chua <89496400+PhenChua29@users.noreply.github.com>
2025-11-02 15:44:41 +0700

    do: update build scripts

--- scripts/docker/build-services.sh
+++ scripts/docker/build-services.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
-services=("website")
+services=(
+  # "website"
+  # "api-gateway"
+  # "signalr"
+  # "tracking-api"
+  # "upload-api"
+  # "identity-api"
+  # "notification-api"
+  # "recipe-api"
+  # "user-api"
+  # "ingredient-predict-api"
+  # "email-worker"
+  # "push-notification-worker"
+  # "recipe-worker"
+  # "sms-worker"
+)
+
 project="tastopia"
 repo="taiduc113/tastopia"
 
-docker compose build website
+echo Building...
+for service in "${services[@]}"; do
+  echo "Building \"${service}\"..."
+  docker compose build ${service}
+done
 
 # Tag each built image into the same repo with different tags
 for service in "${services[@]}"; do
