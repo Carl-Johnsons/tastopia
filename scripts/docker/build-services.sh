109c00e60efb37c56aae494eb204cc9af6c5886b
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2025-11-19 09:45:27 +0700

    do: detect change service in jenkins build pipeline

--- scripts/docker/build-services.sh
+++ scripts/docker/build-services.sh
@@ -3,22 +3,28 @@
 # Exit on failure
 set -e
 
-services=(
-  # "website"
-  # "api-gateway"
-  # "signalr"
-  # "tracking-api"
-  # "upload-api"
+default_services=(
+  "website"
+  "api-gateway"
+  "signalr"
+  "tracking-api"
+  "upload-api"
   "identity-api"
-  # "notification-api"
-  # "recipe-api"
-  # "user-api"
-  # "ingredient-predict-api"
-  # "email-worker"
-  # "push-notification-worker"
-  # "recipe-worker"
-  # "sms-worker"
+  "notification-api"
+  "recipe-api"
+  "user-api"
+  "ingredient-predict-api"
+  "email-worker"
+  "push-notification-worker"
+  "recipe-worker"
+  "sms-worker"
 )
+
+services=("$@")
+
+if [ ${#services[@]} -eq 0 ]; then
+  services=("${default_services[@]}")
+fi
 
 project="tastopia"
 repo="taiduc113/tastopia"
