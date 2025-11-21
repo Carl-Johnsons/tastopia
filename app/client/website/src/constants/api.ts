78addeeb3e676fc42f15f17afc90becf1515c7c8
Phen Chua <89496400+PhenChua29@users.noreply.github.com>
2025-11-21 15:14:54 +0700

    fe: prevent undefined env values

--- app/client/website/src/constants/api.ts
+++ app/client/website/src/constants/api.ts
@@ -1,14 +1,15 @@
 export const SCOPE =
   "openid profile phone email offline_access IdentityServerApi";
 
-export const CLIENT_BASE_URL = process.env.CLIENT_BASE_URL;
+export const CLIENT_BASE_URL = process.env.CLIENT_BASE_URL || "";
 
-export const DUENDE_IDS6_ISSUER = process.env.DUENDE_IDS6_ISSUER;
+export const DUENDE_IDS6_ISSUER = process.env.DUENDE_IDS6_ISSUER || "";
 export const DUENDE_IDENTITY_PROVIDER_NAME = "duende-identity-server6";
-export const DUENDE_IDS6_ID = process.env.DUENDE_IDS6_ID;
+export const DUENDE_IDS6_ID = process.env.DUENDE_IDS6_ID || "";
 
-export const API_GATEWAY_SCHEME = process.env.API_GATEWAY_SCHEME;
-export const API_GATEWAY_HOST = process.env.API_GATEWAY_HOST;
-export const API_GATEWAY_PORT = process.env.API_GATEWAY_PORT;
+export const API_GATEWAY_SCHEME = process.env.API_GATEWAY_SCHEME || "";
+export const API_GATEWAY_HOST = process.env.API_GATEWAY_HOST || "";
+export const API_GATEWAY_PORT = process.env.API_GATEWAY_PORT || "";
+export const API_GATEWAY_PATH = process.env.API_GATEWAY_PATH || "";
 
-export const API_URI = `${API_GATEWAY_SCHEME}://${API_GATEWAY_HOST}:${API_GATEWAY_PORT}`;
+export const API_URI = `${API_GATEWAY_SCHEME}://${API_GATEWAY_HOST}:${API_GATEWAY_PORT}${API_GATEWAY_PATH}`;
