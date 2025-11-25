5889f0f8aedf43ff2ad995e804f5e66f1c517ebb
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2025-11-25 16:36:48 +0700

    do: update deploy script

--- scripts/k8s/deploy.sh
+++ scripts/k8s/deploy.sh
@@ -51,7 +51,7 @@ default_services=(
   "notification-api"
   "recipe-api"
   "user-api"
-  # "ingredient-predict-api"
+  "ingredient-predict-api"
   "email-worker"
   "push-notification-worker"
   "recipe-worker"
