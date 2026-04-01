b2a8852b7468cc0d66f335d610b1e0fb559a9a83
Phen Chua <89496400+PhenChua29@users.noreply.github.com>
2026-04-01 17:52:44 +0700

    do: fix wrong script's path

--- ansible/run.build.sh
+++ ansible/run.build.sh
@@ -2,8 +2,10 @@
 
 set -e
 
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+
 echo "Install ansible plugins"
-ansible-playbook ./playbooks/init-requirements.yml
+ansible-playbook "$SCRIPT_DIR/playbooks/init-requirements.yml"
 
 echo "Run setup scripts"
-ansible-playbook -K site.build.yml
+ansible-playbook -K "$SCRIPT_DIR/site.build.yml"
