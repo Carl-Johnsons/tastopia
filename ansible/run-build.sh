6c60cb6150b9e22eb007866c82c42c090bab1bca
Phen Chua <89496400+PhenChua29@users.noreply.github.com>
2026-05-02 16:41:38 +0700

    o: change file names and restructure folders

---
+++ ansible/run-build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+
+echo "Install ansible plugins"
+ansible-playbook "$script_dir/playbooks/init-build-requirements.yml"
+
+echo "Run setup scripts"
+ansible-playbook -K "$script_dir/site-build.yml"
