14249c4d775464ea50d45786efa89684b878fe55
Phen Chua <89496400+PhenChua29@users.noreply.github.com>
2026-05-07 12:27:36 +0700

    do: add dev env support for website builds

--- scripts/ci/lib/server.sh
+++ scripts/ci/lib/server.sh
@@ -50,6 +50,25 @@ get_api_scheme() {
   echo "$scheme"
 }
 
+get_client_base_url() {
+  : "${WEBSITE_FQDN:?WEBSITE_FQDN is missing}"
+
+  if [ "$ENV" = "dev" ]; then
+    : "${PR_NUMBER:?PR_NUMBER is missing}"
+  fi
+
+  local scheme="${WEBSITE_SCHEME:-https}"
+  local fqdn
+
+  if [ "$ENV" = "dev" ]; then
+    fqdn="pr-$PR_NUMBER-$WEBSITE_FQDN"
+  else
+    fqdn="$WEBSITE_FQDN"
+  fi
+
+  echo "${scheme}://$fqdn"
+}
+
 get_identity_discovery_url() {
   : "${IDENTITY_FQDN:?IDENTITY_FQDN is missing}"
 
@@ -66,4 +85,13 @@ get_identity_discovery_url() {
   fi
 
   echo "$endpoint"
+}
+
+get_duende_ids6_id() {
+  : "${DUENDE_IDS6_ID:?DUENDE_IDS6_ID is missing}"
+  echo "$DUENDE_IDS6_ID"
+}
+
+get_duende_ids6_issuer() {
+  get_identity_discovery_url
 }
