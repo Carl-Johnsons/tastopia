293802edf4427d5545dbfb2f44084ac9fa19fe32
Phen Chua <89496400+PhenChua29@users.noreply.github.com>
2026-05-15 13:36:57 +0700

    do: unify testing variables

---
+++ app/client/mobile/scripts/test-e2e.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euo pipefail
+
+ENV="${ENV:-dev}"
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+export MAESTRO_VERBOSE=true
+
+load_env() {
+  local suffix=""
+  [ "$ENV" != "dev" ] && suffix=".$ENV"
+
+  set -a
+  . "${script_dir}/../.env$suffix"
+  set +a
+}
+
+[ -z "${CI:-}" ] && load_env
+npm run test
