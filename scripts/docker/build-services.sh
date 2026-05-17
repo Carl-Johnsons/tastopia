fa31eb3fec582968616f97899f4ac504dd985ca5
Phen Chua <89496400+PhenChua29@users.noreply.github.com>
2026-05-17 21:52:21 +0700

    do: replace diff-based image building logic with path-based state deriver

--- scripts/docker/build-services.sh
+++ scripts/docker/build-services.sh
@@ -6,7 +6,7 @@ set -eo pipefail
 ENV="staging"
 script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
-while getopts le:c:h OPTS; do
+while getopts let:c:h OPTS; do
   case $OPTS in
     e) 
       if [ "$OPTARG" != "dev" ] && [ "$OPTARG" != "staging" ] && [ "$OPTARG" != "production" ]; then
@@ -16,6 +16,7 @@ while getopts le:c:h OPTS; do
 
       ENV="$OPTARG"
       ;;
+    t) tFlag=1 ;;
     l) lFlag=1 ;;
     c) commitHash="$OPTARG" ;;
     h) cat <<EOF
@@ -27,12 +28,16 @@ Usage: $0 [options] [services]
 
 Options:
   -c [commit]
-        Commit SHA (required)
+        Commit SHA (required in legacy mode)
 
   -e [environment]   
         Specify the environment to build, accepted values
         are "dev", "staging" or "production". If omitted, 
         the default value is "staging".
+
+  -t    Turn on tag reading mode. Inputs are now treated
+        with the assumed format "<service>:<tag>". For
+        example, "identity-api:be06a86d".
 
   -l    Load env file based on the current specified
         environment.
@@ -43,35 +48,23 @@ EOF
       exit 0
       ;;
     ?) 
-      echo "Unknown flag. Usage: $0 -c <commit> [-l] [-e dev|staging|production] [services]"
+      echo "Unknown flag. Usage: $0 [-c <commit>] [-lth] [-e dev|staging|production] [services]"
       exit 1
       ;;
   esac
 done
 
-if [ -z "$commitHash" ]; then
-  echo "Error: -c <commit> is required."
-  echo "Usage: $0 -c <commit> [-e dev|staging|production] [-l] [services]"
-  exit 1
-fi
-
 # Shift parsed options
 shift $((OPTIND - 1))
 
-export ENV
+services=("$@")
+project="$(basename $(pwd))"
+project="${project,,}" # make the name lowercase
+repo="taiduc113/tastopia"
 
-if [ -n "$lFlag" ]; then
-  SUFFIX="" 
-
-  if [ "$ENV" != "dev" ]; then
-    SUFFIX=".$ENV"
-  fi
-
-  set -a
-  . "$script_dir/../../.env$SUFFIX"
-  set +a
-  echo "loaded .env$SUFFIX file"
-  unset SUFFIX
+if [ ${#services[@]} -eq 0 ]; then
+  echo Please specify the services to build >&2
+  exit 1
 fi
 
 default_services=(
@@ -91,87 +84,177 @@ default_services=(
   "sms-worker"
 )
 
-dotnet_services=(
-  "api-gateway"
-  "signalr"
-  "tracking-api"
-  "upload-api"
-  "identity-api"
-  "notification-api"
-  "recipe-api"
-  "user-api"
-  "email-worker"
-  "push-notification-worker"
-  "recipe-worker"
-  "sms-worker"
-)
-
-services=("$@")
-
-if [ ${#services[@]} -eq 0 ]; then
-  echo Please specify the services to build >&2
-  exit 1
-fi
-
-project="$(basename $(pwd))"
-project="${project,,}" # make the name lowercase
-repo="taiduc113/tastopia"
-
-# Always build contract image first
-CONTRACT_HASH=$(git log -n 1 --pretty=format:%H -- app/server/Contract) # Get latest commit full SHA that touches app/server/Contract
-CONTRACT_REBUILT=0
-if docker manifest inspect ${repo}-contract:${CONTRACT_HASH} > /dev/null 2>&1; then
-  echo "Contract image with hash ${CONTRACT_HASH} exists → skip"
-else
-  echo "Contract image with hash ${CONTRACT_HASH} not found → build"
-  docker compose build contract
-  docker tag ${project}-contract ${repo}-contract:${CONTRACT_HASH}
-  docker push ${repo}-contract:${CONTRACT_HASH}
-  CONTRACT_REBUILT=1
-fi
-
-if [ "$CONTRACT_REBUILT" -eq 1 ]; then
-  echo "Contract was rebuilt → forcing rebuild of all dotnet services"
-  # Add all dotnet services to the services array if not already present
-  for dotnet_service in "${dotnet_services[@]}"; do
-    if [[ ! " ${services[@]} " =~ " ${dotnet_service} " ]]; then
-      services+=("$dotnet_service")
-      echo "Added ${dotnet_service} to build list"
-    fi
-  done
-fi
-
-# Tag and push built images if they don't already exist on Docker Hub
-for service in "${services[@]}"; do
-  if ! printf '%s\n' "${default_services[@]}" | grep -qxF "$service"; then
-    continue
+# Build services in the standard way. In this mode, inputs are expected to be a 
+# simple list. For example, "website recipe-worker identity-api".
+#
+# This mode is used to build images with the provided -c <commitHash> as their tags.
+build_services_legacy() {
+  if [ -z "$commitHash" ]; then
+    echo "Error: -c <commit> is required."
+    echo "Usage: $0 -c <commit> [-e dev|staging|production] [-l] [services]"
+    exit 1
   fi
 
-  serviceRepo=${repo}-${service}
+  dotnet_services=(
+    "api-gateway"
+    "signalr"
+    "tracking-api"
+    "upload-api"
+    "identity-api"
+    "notification-api"
+    "recipe-api"
+    "user-api"
+    "email-worker"
+    "push-notification-worker"
+    "recipe-worker"
+    "sms-worker"
+  )
 
-  if [ "$service" = "website" ]; then
-    tag="${ENV}-${commitHash}"
+  # Always build contract image first
+  CONTRACT_HASH=$(git log -n 1 --pretty=format:%H -- app/server/Contract) # Get latest commit full SHA that touches app/server/Contract
+  CONTRACT_REBUILT=0
+  if docker manifest inspect ${repo}-contract:${CONTRACT_HASH} > /dev/null 2>&1; then
+    echo "Contract image with hash ${CONTRACT_HASH} exists → skip"
   else
-    tag="${commitHash}"
+    echo "Contract image with hash ${CONTRACT_HASH} not found → build"
+    docker compose build contract
+    docker tag ${project}-contract ${repo}-contract:${CONTRACT_HASH}
+    docker push ${repo}-contract:${CONTRACT_HASH}
+    CONTRACT_REBUILT=1
   fi
 
-  IMAGE="${serviceRepo}:${tag}"
-
-  if docker manifest inspect "$IMAGE" > /dev/null 2>&1; then
-    echo "Image ${IMAGE} already exists on Docker Hub → skipping build and push"
-    continue
+  if [ "$CONTRACT_REBUILT" -eq 1 ]; then
+    echo "Contract was rebuilt → forcing rebuild of all dotnet services"
+    # Add all dotnet services to the services array if not already present
+    for dotnet_service in "${dotnet_services[@]}"; do
+      if [[ ! " ${services[@]} " =~ " ${dotnet_service} " ]]; then
+        services+=("$dotnet_service")
+        echo "Added ${dotnet_service} to build list"
+      fi
+    done
   fi
 
-  echo "Building and pushing \"${service}\" to ${IMAGE}..."
+  # Tag and push built images if they don't already exist on Docker Hub
+  for service in "${services[@]}"; do
+    if ! printf '%s\n' "${default_services[@]}" | grep -qxF "$service"; then
+      continue
+    fi
+
+    serviceRepo=${repo}-${service}
+
+    if [ "$service" = "website" ]; then
+      tag="${ENV}-${commitHash}"
+    else
+      tag="${commitHash}"
+    fi
+
+    image="${serviceRepo}:${tag}"
+
+    if docker manifest inspect "$image" > /dev/null 2>&1; then
+      echo "Image ${image} already exists on Docker Hub → skipping build and push"
+      continue
+    fi
+
+    echo "Building and pushing \"${service}\" to ${image}..."
+    
+    if [ "$service" = "website" ] && [ "$ENV" = "dev" ]; then
+      echo "Preparing website env for dev build..."
+      "$script_dir/../ci/build/setup-website-env.sh"
+    fi
+
+    docker compose build ${service} --build-arg CONTRACT_IMAGE=${repo}-contract:$CONTRACT_HASH 2>&1 | tee build.log
+    
+    echo "Pushing ${image}..."
+    docker tag ${project}-${service} ${image}
+    docker push ${image}
+  done
+}
+
+load_env() {
+  export ENV
+
+  if [ -n "$lFlag" ]; then
+    SUFFIX="" 
+
+    if [ "$ENV" != "dev" ]; then
+      SUFFIX=".$ENV"
+    fi
+
+    set -a
+    . "$script_dir/../../.env$SUFFIX"
+    set +a
+    echo "loaded .env$SUFFIX file"
+    unset SUFFIX
+  fi
+}
+
+# Build services using a list with predetermined tag for each services.
+# Inputs are now treated with the assumed format "<service>:<tag>". For 
+# example, "identity-api:be06a86d recipe-api:999f9f12".
+#
+# This mode allow services to be built with their respective tag, allowing 
+# the image tags to reflect the right commit sha that modified the service.
+build_services() {
+  # Always build contract image first
+  if grep -q 'contract' <<< "${services[*]}" 2>/dev/null; then
+    image=$(printf '%s\n' "${services[@]}" | grep '^contract:')
+    contract_hash="${image#*:}"
+  else
+    contract_hash="$(git log -n 1 --pretty=format:%H -- app/server/Contract)"
+  fi
+
+  if docker manifest inspect ${repo}-contract:${contract_hash} &>/dev/null; then
+    echo "Contract image with hash ${contract_hash} exists → skip"
+  else
+    echo "Contract image with hash ${contract_hash} not found → build"
+    docker compose build contract
+    docker tag ${project}-contract ${repo}-contract:${contract_hash}
+    docker push ${repo}-contract:${contract_hash}
+  fi
   
-  if [ "$service" = "website" ] && [ "$ENV" = "dev" ]; then
-    echo "Preparing website env for dev build..."
-    "$script_dir/../ci/build/setup-website-env.sh"
-  fi
+  # Tag and push built images if they don't already exist on Docker Hub
+  for image in "${services[@]}"; do
+    service="${image%%:*}"
 
-  docker compose build ${service} --build-arg CONTRACT_IMAGE=${repo}-contract:$CONTRACT_HASH 2>&1 | tee build.log
-  
-  echo "Pushing ${IMAGE}..."
-  docker tag ${project}-${service} ${IMAGE}
-  docker push ${IMAGE}
-done
+    if ! printf '%s\n' "${default_services[@]}" | grep -qxF "$service"; then
+      continue
+    fi
+
+    commitHash="${image#*:}"
+    serviceRepo=${repo}-${service}
+
+    if [ "$service" = "website" ]; then
+      tag="${ENV}-${commitHash}"
+    else
+      tag="${commitHash}"
+    fi
+
+    image="${serviceRepo}:${tag}"
+
+    if docker manifest inspect "$image" > /dev/null 2>&1; then
+      echo "Image ${image} already exists on Docker Hub → skipping build and push"
+      continue
+    fi
+
+    echo "Building and pushing \"${service}\" to ${image}..."
+    
+    if [ "$service" = "website" ] && [ "$ENV" = "dev" ]; then
+      echo "Preparing website env for dev build..."
+      "$script_dir/../ci/build/setup-website-env.sh"
+    fi
+
+    docker compose build ${service} --build-arg CONTRACT_IMAGE=${repo}-contract:$contract_hash 2>&1 | tee build.log
+    
+    echo "Pushing ${image}..."
+    docker tag ${project}-${service} ${image}
+    docker push ${image}
+  done
+}
+
+load_env
+
+if [ -n "${tFlag:-}" ]; then
+  build_services
+else
+  build_services_legacy
+fi
