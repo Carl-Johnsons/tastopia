e6da57dfe1b49605fc5477e2b84035daa24ac7db
Phen Chua <89496400+PhenChua29@users.noreply.github.com>
2026-05-18 17:03:55 +0700

    do: add windows support for testing deps

--- ansible/run-test.sh
+++ ansible/run-test.sh
@@ -4,8 +4,20 @@ set -e
 
 script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
+init_playbook="init-test-requirements.yml"
+site_playbook="site-test.yml"
+
+if [[ "$OSTYPE" == msys* ]] \
+  || [[ "$OSTYPE" == cygwin* ]] \
+  || [[ "$OSTYPE" == win32* ]]; then
+  echo "Windows detected"
+  init_playbook="init-test-requirements-windows.yml"
+  site_playbook="site-test-windows.yml"
+fi
+
 echo "Install ansible plugins"
-ansible-playbook "$script_dir/playbooks/init-test-requirements.yml"
+ansible-playbook "$script_dir/playbooks/$init_playbook"
 
 echo "Run setup scripts"
-ansible-playbook -K "$script_dir/site-test.yml"
+ansible-playbook -K "$script_dir/$site_playbook"
+
